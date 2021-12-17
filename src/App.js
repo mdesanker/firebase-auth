e8e01982dc8373c197884056df082411b0e67756
Michael Desanker <mdesanker@gmail.com>
2021-12-17 12:22:50 -0500

    Create login fields

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 const App = () => {
-  return <div>Hello, world!</div>;
+  const loginHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={loginHandler}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
